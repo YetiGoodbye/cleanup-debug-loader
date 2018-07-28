ffe11fd9ad18a475355f11bde41e5e7d9567700f
YetiGoodbye <tar.vxf@gmail.com>
2018-07-29 01:03:31 +0500

    can strip lines and blocks

--- index.js
+++ index.js
@@ -1,7 +1,4 @@
-const getOptions = require('loader-utils').getOptions;
 const validateOptions = require('schema-utils');
-
-const defaultMarker = "@#";
 
 const schema = {
   type: 'object',
@@ -15,29 +12,43 @@ const whiteSpace = "\s*";
 const restLine = "\s*";
 
 function cleanup(source){
-  
-  console.log("\n\n\n---------------------------------------");
-  // console.log(this._compilation.compiler.options.mode);
+
   const devmode = (this._compilation.compiler.options.mode === "development");
   
-  var options = getOptions(this);
+  var options = this.query;
   options = options || {};
-  options.marker = options.marker || defaultMarker;
-  options.dropInDev = options.dropInDev || '+';
-  options.dropInProd = options.dropInProd || '-';
+  const m = options.marker = options.marker || '@#';
+  const dd = options.dropInDev = options.dropInDev || '[+]';
+  const dp = options.dropInProd = options.dropInProd || '[-]';
 
   validateOptions(schema, options, 'Clenup Debug Loader');
-  console.log(options);
 
-  const dropInDev  = RegExp(`((^|\n)\s*)${options.marker}[${options.dropInDev}](.*)`,  'g');
-  const dropInProd = RegExp(`((^|\n)\s*)${options.marker}[${options.dropInProd}](.*)`, 'g');
+  const dropInDev  =     RegExp(`(^\s*)${m}${dd}(.*)`,  'gm');
 
-  // /((^|\n)\s*)@@[$](.*)/g
+  const dropBlockInDev  = RegExp(
+    '^\\s*' + m + dd + '\\[\\s*' +
+    '(\\n(.*\\n)*)\\s*' + m + dd + '\\]\\s*\\n',
+    'gm');
+
+  const dropInProd =      RegExp(`(^\s*)${m}${dp}(.*)`, 'gm');
+
+  const dropBlockInProd = RegExp(
+    '^\\s*' + m + dp + '\\[\\s*' +
+    '(\\n(.*\\n)*)\\s*' + m + dp + '\\]\\s*\\n',
+    'gm');
 
   if(devmode){
-    return source.replace(dropInDev, '').replace(dropInProd,'$1$3');
+    return source
+      .replace(dropBlockInDev, '')
+      .replace(dropBlockInProd, '$1')
+      .replace(dropInDev, '')
+      .replace(dropInProd,'$1$2');
   } else {
-    return source.replace(dropInProd, '').replace(dropInDev,'$1$3');
+    return source
+      .replace(dropBlockInDev, '$1')
+      .replace(dropBlockInProd, '')
+      .replace(dropInDev,'$1$2')
+      .replace(dropInProd, '');
   }
 }
 
